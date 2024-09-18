ab54d7e7f8ef1904270eed142e32e46babe90195
Regina Massarova <rmassarova@sipuni.ru>
2024-09-18 14:48:32 +0400

    new_commit

--- tests.py
+++ tests.py
@@ -22,3 +22,83 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+
+import pytest
+
+class TestBooksCollector:
+
+    # Т
+    @pytest.mark.parametrize("book_name", [
+        ("Гарри Поттер и философский камень"),
+        ("Властелин колец"),
+        ("1984"),
+    ])
+    def test_add_new_book(self, book_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        assert len(collector.get_books_genre()) == 1
+        assert book_name in collector.get_books_genre()
+
+    #
+    def test_set_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и философский камень')
+        collector.set_book_genre('Гарри Поттер и философский камень', 'Фантастика')
+        assert collector.get_book_genre('Гарри Поттер и философский камень') == 'Фантастика'
+
+    #
+    def test_set_book_genre_for_non_existent_book(self):
+        collector = BooksCollector()
+        collector.set_book_genre('Незарегистрированная книга', 'Фантастика')
+        assert collector.get_book_genre('Незарегистрированная книга') is None
+
+    #
+    @pytest.mark.parametrize("genre, expected_books", [
+        ('Фантастика', ['Гарри Поттер и философский камень']),
+        ('Ужасы', ['Сияние']),
+    ])
+    def test_get_books_with_specific_genre(self, genre, expected_books):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и философский камень')
+        collector.set_book_genre('Гарри Поттер и философский камень', 'Фантастика')
+        collector.add_new_book('Сияние')
+        collector.set_book_genre('Сияние', 'Ужасы')
+        assert collector.get_books_with_specific_genre(genre) == expected_books
+
+
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и философский камень')
+        collector.set_book_genre('Гарри Поттер и философский камень', 'Фантастика')
+        collector.add_new_book('Сияние')
+        collector.set_book_genre('Сияние', 'Ужасы')
+        assert collector.get_books_for_children() == ['Гарри Поттер и философский камень']
+
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и философский камень')
+        collector.add_book_in_favorites('Гарри Поттер и философский камень')
+        assert 'Гарри Поттер и философский камень' in collector.get_list_of_favorites_books()
+
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и философский камень')
+        collector.add_book_in_favorites('Гарри Поттер и философский камень')
+        collector.delete_book_from_favorites('Гарри Поттер и философский камень')
+        assert 'Гарри Поттер и философский камень' not in collector.get_list_of_favorites_books()
+
+
+    def test_add_new_book_with_long_name(self):
+        collector = BooksCollector()
+        long_name = 'Очень длинное название книги, которое превышает сорок символов'
+        collector.add_new_book(long_name)
+        assert long_name not in collector.get_books_genre()
+
+    def test_add_duplicate_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и философский камень')
+        collector.add_new_book('Гарри Поттер и философский камень')
+        assert len(collector.get_books_genre()) == 1
